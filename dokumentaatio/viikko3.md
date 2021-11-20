348652c2b97fdd394254c2906ddbca4c3f2a6974
Mvaaras <mvaaras@gmail.com>
2021-11-20 20:50:14 +0200

    Viikon 3 palautus

---
+++ dokumentaatio/viikko3.md
@@ -0,0 +1,13 @@
+# Viikkoraportti - Viikko 3
+
+*Käytetty aika ~6 tuntia josta valtaosa paperilla tehtyä suunnittelutyötä*
+
+Tällä viikolla olen pohtinut erilaisia tapoja arvioida peliruudukkoa ja erilaisia tilanteita. Olen päätynyt keinoon jota haluan lähteä toteuttamaan, mutta toteutus on vielä aika vaiheessa.
+
+Ohjelma ei ole edistynyt paljoa koska olen ei niin yllättäen ollut taas kiireinen (selvisi että tässä jaksossa minulla tulee olemaan paljon rajoitetummin aikaa kuin mitä osasin odottaa käytännössä koko jakson ajan). Käytännössä kaikki sinne tehdyt muutokset ovat vielä minimaalisia ja kesken. Mutta suunnitelma siitä miten projektin olisi edettävä on tarkempi kuin aiemmin.
+
+Tällä viikolla en ole varma, opinko varsinaisesti mitään, mutta ymmärrän nyt paremmin pelin rakennetta ja sitä, miten lautaa voisi kenties lähteä analysoimaan.
+
+Minulle ei jäänyt mitään kauhean epäselvää mieleen.
+
+Seuraavaksi aion jatkaa sen algoritmin toteutusta ja laudan arviointihomman tekoa paremman suunnitelman johdolla.
--- gomoku_ai.py
+++ gomoku_ai.py
@@ -1,3 +1,25 @@
+rivit = {
+#näitä vedetty hatusta, vähän sinne päin ja niin edelleen. Millaiset rivit hyviä?
+[1,1,1,1,1]:99,
+[0,1,1,1,1]:90,
+[-1,1,1,1,1]:4,
+
+[0,1,1,1,0]:90,
+[0,1,1,0]: 2,
+[0,1,0,1,0]: 2,
+
+[-1,1,1,1,0,1]:4,
+[0,1,1,0,1,1]:4,
+[-1,1,1,0,1,1]:4,
+
+[-1,1,1,1]:3,
+[1,1,0,1]:3,
+[1,1,0,1]:kolme,
+
+
+}
+#Korvaa omalla systeemillä myöhemmin? Dict on kiva :(
+
 class minimaxeri():
 
     def __init__(self,syvyys,koko,arvo):
@@ -5,16 +27,27 @@ class minimaxeri():
         self.koko = koko
         self.arvo = arvo
         self.lauta = [[0 for i in range(koko)] for j in range(koko)]
+        self.arvioija = [0 for i in range(5)]
 
-    def arvioi(lauta):
-        #TODO
-        #Koodi jollain tavoin arvioi laudan ja kykenee pisteyttämään eri tilanteita
-        #Esim viiden rivit ja estämättömät neljän rivi pisteytetään todella korkeiksi koska ovat varmoja voittotilanteita  
-        #(toistetaan useita, useita kertoja haun aikana)
-        
-        #Palauttaa rivipaikat ja arvot jossain muodossa?
+    def arvioi(self, lauta,vuoro):
+        laudan_arvo = 0
+        for i in range(self.koko):
+            for j in range(self.koko):
+                if lauta[i][j] == vuoro:
+                    laudan_arvo += self.arvio_apuri(j,i)
         
         return
+    
+    def arvio_apuri(self, x, y, vuoro):
+        rivien_arvo = 0
+        #vasemmalta oikealle:
+        if not x == 0 or self.lauta[y][x-1] == 0:
+            katki_vasen = False
+        else: katki_vasen = True
+        self.arvioija[0] = 1
+            
+
+
 
     def minimax_alku(self,lauta):
         #TODO
@@ -31,8 +64,9 @@ class minimaxeri():
 
         if syvyys <= 0: return paras
 
-        self.arvioidut = self.arvioi(self.lauta)
+        
         if oma_vuoro:
+            self.arvioidut = self.arvioi(self.lauta,self.arvo)
             for i in self.arvioidut:
 
 # Tämä koko osuus vielä erittäin työn alla
